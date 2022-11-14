026554b6bb3b487104cc2359314973b34c199341
E-K8 <ekatplat@gmail.com>
2022-11-14 21:21:13 +0000

    refactor tests with test suites

--- basic-testing-frontend/src/util/validation.test.js
+++ basic-testing-frontend/src/util/validation.test.js
@@ -1,97 +1,101 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 import { validateStringNotEmpty, validateNumber } from './validation';
 
-it('should throw an error when an empty string is provided', () => {
-  // Arrange
-  const input = '';
+describe('validateStringNotEmpty()', () => {
+  it('should throw an error when an empty string is provided', () => {
+    // Arrange
+    const input = '';
 
-  // Act
-  const validationFn = () => validateStringNotEmpty(input);
+    // Act
+    const validationFn = () => validateStringNotEmpty(input);
 
-  // Assert
-  expect(validationFn).toThrow();
+    // Assert
+    expect(validationFn).toThrow();
+  });
+
+  it('should throw an error with a message that contains a reason (must not be empty)', () => {
+    // Arrange
+    const input = '';
+
+    // Act
+    const validationFn = () => validateStringNotEmpty(input);
+
+    // Assert
+    expect(validationFn).toThrow(/must not be empty/);
+  });
+
+  it('should throw an error when a long string of blanks is provided', () => {
+    // Arrange
+    const input = '     ';
+
+    // Act
+    const validationFn = () => validateStringNotEmpty(input);
+
+    // Assert
+    expect(validationFn).toThrow();
+  });
+
+  it('should throw an error if any other value other than a string is provided', () => {
+    // Arrange
+    const inputNum = 1;
+    const inputBool = true;
+    const inputObj = {};
+
+    // Act
+    const validationFnNum = () => validateStringNotEmpty(inputNum);
+    const validationFnBool = () => validateStringNotEmpty(inputBool);
+    const validationFnObj = () => validateStringNotEmpty(inputObj);
+
+    // Assert
+    expect(validationFnNum).toThrow();
+    expect(validationFnBool).toThrow();
+    expect(validationFnObj).toThrow();
+  });
+
+  it('should not throw an error when a non-empty string is provided', () => {
+    // Arrange
+    const input = 'valid';
+
+    // Act
+    const validationFn = () => validateStringNotEmpty(input);
+
+    // Assert
+    expect(validationFn).not.toThrow();
+  });
 });
 
-it('should throw an error with a message that contains a reason (must not be empty)', () => {
-  // Arrange
-  const input = '';
+describe('validateNumber()', () => {
+  it('should throw an error if NaN is provided', () => {
+    // Arrange
+    const input = NaN;
 
-  // Act
-  const validationFn = () => validateStringNotEmpty(input);
+    // Act
+    const validationFn = () => validateNumber(input);
 
-  // Assert
-  expect(validationFn).toThrow(/must not be empty/);
-});
+    // Assert
+    expect(validationFn).toThrow();
+  });
 
-it('should throw an error when a long string of blanks is provided', () => {
-  // Arrange
-  const input = '     ';
+  it('should throw an error with a message that contains a reason (invalid number)', () => {
+    // Arrange
+    const input = NaN;
 
-  // Act
-  const validationFn = () => validateStringNotEmpty(input);
+    // Act
+    const validationFn = () => validateNumber(input);
 
-  // Assert
-  expect(validationFn).toThrow();
-});
+    // Assert
+    expect(validationFn).toThrow(/Invalid number/);
+  });
 
-it('should throw an error if any other value other than a string is provided', () => {
-  // Arrange
-  const inputNum = 1;
-  const inputBool = true;
-  const inputObj = {};
+  it('should throw an error if a non-numeric value is provided', () => {
+    const input = '1';
+    const validationFn = () => validateNumber(input);
+    expect(validationFn).toThrow();
+  });
 
-  // Act
-  const validationFnNum = () => validateStringNotEmpty(inputNum);
-  const validationFnBool = () => validateStringNotEmpty(inputBool);
-  const validationFnObj = () => validateStringNotEmpty(inputObj);
-
-  // Assert
-  expect(validationFnNum).toThrow();
-  expect(validationFnBool).toThrow();
-  expect(validationFnObj).toThrow();
-});
-
-it('should not throw an error when a non-empty string is provided', () => {
-  // Arrange
-  const input = 'valid';
-
-  // Act
-  const validationFn = () => validateStringNotEmpty(input);
-
-  // Assert
-  expect(validationFn).not.toThrow();
-});
-
-it('should throw an error if NaN is provided', () => {
-  // Arrange
-  const input = NaN;
-
-  // Act
-  const validationFn = () => validateNumber(input);
-
-  // Assert
-  expect(validationFn).toThrow();
-});
-
-it('should throw an error with a message that contains a reason (invalid number)', () => {
-  // Arrange
-  const input = NaN;
-
-  // Act
-  const validationFn = () => validateNumber(input);
-
-  // Assert
-  expect(validationFn).toThrow(/Invalid number/);
-});
-
-it('should throw an error if a non-numeric value is provided', () => {
-  const input = '1';
-  const validationFn = () => validateNumber(input);
-  expect(validationFn).toThrow();
-});
-
-it('should not throw an error, if a number is provided', () => {
-  const input = 1;
-  const validationFn = () => validateNumber(input);
-  expect(validationFn).not.toThrow();
+  it('should not throw an error, if a number is provided', () => {
+    const input = 1;
+    const validationFn = () => validateNumber(input);
+    expect(validationFn).not.toThrow();
+  });
 });
