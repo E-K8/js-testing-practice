d3a7d3a1b50bf40e89cc91d0123459b9d8234674
E-K8 <ekatplat@gmail.com>
2022-12-19 22:32:50 +0000

    test hooks beforeAll, beforeEach, afterEach, afterAll

--- asynchronous/hooks/hooks.test.js
+++ asynchronous/hooks/hooks.test.js
@@ -1,46 +1,54 @@
-import { it, expect } from 'vitest';
+import { it, expect, beforeAll, beforeEach, afterAll, afterEach } from 'vitest';
 
 import { User } from './hooks';
 
+const testEmail = 'test@test.com';
+let user; // it makes sense to create a global variable when there are lots of tests and test suites
+
+beforeAll(() => {
+  user = new User(testEmail);
+  console.log('beforeAll()');
+});
+
+beforeEach(() => {
+  console.log('beforeEach()');
+});
+
+afterEach(() => {
+  user = new User(testEmail);
+
+  console.log('afterEach()');
+});
+
+afterAll(() => {
+  console.log('afterAll()');
+});
+
+// afterAll is good for cleanup work, for example erase a temporary database that was only created to test stuff
+
 it('should update the email', () => {
-  const testEmail = 'test@test.com';
   const newTestEmail = 'test2@test.com';
 
-  const user = new User(testEmail);
   user.updateEmail(newTestEmail);
 
   expect(user.email).toBe(newTestEmail);
 });
 
 it('should have an email property', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
-
   expect(user).toHaveProperty('email');
 });
 
 it('should store the provided email value', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
-
   expect(user.email).toBe(testEmail);
 });
 
 it('should clear the email', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
   user.clearEmail();
 
   expect(user.email).toBe('');
 });
 
 it('should still have an email property after clearing the email', () => {
-  const testEmail = 'test@test.com';
-
-  const user = new User(testEmail);
   user.clearEmail();
 
   expect(user).toHaveProperty('email');
