ee772d31f12a8e81418b005919d864843babe18b
E-K8 <ekatplat@gmail.com>
2022-12-22 21:43:07 +0000

    add spy test to check if a function is called

---
+++ side-effects/src/data.test.js
@@ -0,0 +1,11 @@
+import { describe, it, expect, vi } from 'vitest';
+import { generateReportData } from './data';
+
+describe('generate report data()', () => {
+  it('should execute logFn if provided', () => {
+    const logger = vi.fn(); // creates an empty function that keeps track of executions of that function and arguments that were provided with those calls
+
+    generateReportData(logger);
+    expect(logger).toBeCalled();
+  });
+});
