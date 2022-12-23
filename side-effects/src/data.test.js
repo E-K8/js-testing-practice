42fe75a1a65923a74ed955a713abc7fe6dfb3d05
E-K8 <ekatplat@gmail.com>
2022-12-23 19:46:00 +0000

    add custom mocking logic with .toBeCalledWith and path

--- side-effects/src/data.test.js
+++ side-effects/src/data.test.js
@@ -6,6 +6,9 @@ describe('generate report data()', () => {
     const logger = vi.fn(); // creates an empty function that keeps track of executions of that function and arguments that were provided with those calls
 
     generateReportData(logger);
+
     expect(logger).toBeCalled();
+
+    // expect(logger).toBeCalledTimes(2);
   });
 });
--- side-effects/src/util/io.test.js
+++ side-effects/src/util/io.test.js
@@ -3,13 +3,26 @@ import { promises as fs } from 'fs';
 import writeData from './io';
 vi.mock('fs'); // test.txt won't be created, all functions will be replaced with empty modules, we no longer will get back a promise that gets resolved
 
+// also, vi.mock() gets hoisted behind the scenes to the top of the file and is execuded before imports
+
+vi.mock('path', () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
+
 it('should execute the writeFile method', () => {
   const testData = 'Test';
   const testFilename = 'test.txt';
 
   writeData(testData, testFilename);
   // expect(writeData(testData, testFilename)).resolves.toBeUndefined();
-  expect(fs.writeFile).toBeCalled();
+  // expect(fs.writeFile).toBeCalled();
+  expect(fs.writeFile).toBeCalledWith(testFilename, testData);
 });
 
 // side effect here could be creation or deletion of files that we don't intend to have on the system or vice versa
